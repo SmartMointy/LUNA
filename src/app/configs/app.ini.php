14ac0ff111bedf961e1297f8e4058e2374d3d15c
Mointy <56931663+SmartMointy@users.noreply.github.com>
2020-07-08 16:15:06 +0200

    Update app.ini.php

--- src/app/configs/app.ini.php
+++ src/app/configs/app.ini.php
@@ -9,7 +9,7 @@ return array(
         'default' => 'default_icon_path',
         'safari' => 'safari_icon_path'
     ],
-    'version' => '2.7.9',
+    'version' => '0.1.0',
     'mode' => 'developement',
     'language' => 'de',
     'all_languages' => ['de', 'en', 'fr', 'tr'],
